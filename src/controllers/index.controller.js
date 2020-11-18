1458b476219fff489cf4de460d2173a210aa83a6
mcoronado <matias.coronado@usach.cl>
2020-11-18 18:57:13 -0300

    Test + fix de controlador

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -23,13 +23,12 @@ const getPermisos = async(req,res) => {
 }
 
 const createPermiso = async (req,res)=> {
-    const {run,nombre,direccion,motivo,email} = req.body;
-    const date = new Date();
-
-    if(verificarPermiso(req,date)){
+    const verif = await verificarPermiso(req)
+    if(verif){
+        const {run,nombre,direccion,motivo,email} = req.body;
+        const date = new Date();
         const fecha_inicio = new Date(date.getTime()+15*60000);
         const fecha_termino = new Date(fecha_inicio.getTime()+120*60000);
-
         const resp = await pool.query('INSERT INTO permisos VALUES (DEFAULT,$1,$2,$3,$4,$5,$6,$7)',[run,nombre,direccion,motivo,email,fecha_inicio,fecha_termino]).catch(function (error) {
             if (error.response) {
                 res.json({
@@ -37,15 +36,9 @@ const createPermiso = async (req,res)=> {
                 });
             }
         });
-        const resid = await pool.query('SELECT id FROM permisos WHERE run =$1 AND nombre=$2 AND direccion=$3 AND fechaInicio=$4 LIMIT 1',[run,nombre,direccion,fecha_inicio]).catch(function (error) {
-            if (error.response) {
-                res.json({
-                    message: 'Ocurrio un error al generar su permiso'
-                });
-            }
-        });    
-        id = resid.rows[0].id
-        const permiso = generarPdf(req,id,fecha_inicio,fecha_termino)
+
+        id = await getId(req,fecha_inicio)
+        const permiso = await generarPdf(req,id,fecha_inicio,fecha_termino)
         if(sendPermiso(email,permiso)){
             res.json({
                 message: 'Permiso generado exitosamente! \nID del permiso: '+id+'\nFecha de inicio: '+fecha_inicio.toString()+'\nFecha de termino: '+fecha_termino.toString(),
@@ -66,32 +59,45 @@ const createPermiso = async (req,res)=> {
     }
 };
 
-
-function verificarPermiso (req,date){
-    /*
+async function getId(req,fechaInicio){
     const {run,nombre,direccion,motivo,email} = req.body;
-    const resp = await pool.query('SELECT fechaInicio FROM permisos WHERE run =$1 ORDER BY id DESC LIMIT 1 ',[run]).catch(function (error) {
+    const resid = await pool.query('SELECT id FROM permisos WHERE run =$1 AND nombre=$2 AND direccion=$3 AND fechaInicio=$4 LIMIT 1',[run,nombre,direccion,fechaInicio]).catch(function (error) {
+        if (error.response) {
+            res.json({
+                message: 'Ocurrio un error al generar su permiso'
+            });
+        }
+    });  
+    return resid.rows[0].id 
+}
+
+
+async function verificarPermiso (req){
+    const {run} = req.body;
+    const resp = await pool.query('SELECT fechaInicio FROM permisos WHERE run = $1 AND fechaInicio > now() - interval \'1 day\' ORDER BY id DESC LIMIT 1  ',[run]).catch(function (error) {
         if (error.response) {
             res.json({
                 message: 'Ocurrio un error al generar su permiso'
             });
         }
     });
-    if(resp.rowCount== 0){
+    console.log(resp)
+    try {
+        rowCount = resp.rowCount
+    } catch (error) {
+        rowCount = 0
+    }
+    if(rowCount == 0){
         return true
     }else{
-        console.log(resp)
-        console.log(resp.rows[0])
-        console.log(resp.rows[0].fechaInicio.toString())  
+        return false
     }
-    */
-   return true
 }
 
 
 
-function generarPdf(req,id,fecha_inicio,fecha_termino){
-    const {run,nombre,direccion,motivo,email} = req.body;
+async function generarPdf(req,id,fecha_inicio,fecha_termino){
+    const {run,nombre,direccion,motivo} = req.body;
     const doc = new PDFDocument();
     doc.pipe(fs.createWriteStream('permiso'+run+'.pdf'));
     doc
@@ -119,6 +125,7 @@ function generarPdf(req,id,fecha_inicio,fecha_termino){
         width: 410,
         align: 'center'})    
     doc.end();
+    console.log('se creo')
     return 'permiso'+run+'.pdf'
 }
 
@@ -156,6 +163,14 @@ function sendPermiso (email, permiso){
 }
 
 
+function deletePermiso(permiso){
+    try{
+    fs.unlink(permiso, function (err) {
+        if (err) throw err;
+        // if no error, file has been deleted successfully
+    });  }catch(error){
+    }
+}
 
 
 // Exports
