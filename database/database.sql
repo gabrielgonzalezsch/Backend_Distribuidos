9c1c1c20e1d7ef0c318bc4842b76d10115e08af1
mcoronado <matias.coronado@usach.cl>
2020-11-18 02:40:04 -0300

    Cambio 1 + test de back(80%)

--- database/database.sql
+++ database/database.sql
@@ -2,16 +2,17 @@ DROP DATABASE IF EXISTS comisaria;
 CREATE DATABASE comisaria;
 
 
-
 CREATE TABLE permisos(
     id SERIAL PRIMARY KEY,
     run VARCHAR(30) NOT NULL,
     nombre VARCHAR(60) NOT NULL,
     direccion VARCHAR(60) NOT NULL,
     motivo VARCHAR(40) NOT NULL,
-    fecha TIMESTAMP NOT NULL
+    email VARCHAR(40) NOT NULL,
+    fechaInicio TIMESTAMP NOT NULL,
+    fechaTermino TIMESTAMP NOT NULL
 );
 
 
-INSERT INTO permisos VALUES ('192453356','gabriel','av siempre viva','me pesan las weas','2001-02-16 20:38:40');
+INSERT INTO permisos VALUES (DEFAULT,'192453356','gabriel','av siempre viva','me pesan las weas','matias.coronado@usach.cl','2001-02-16 20:38:40','2002-02-16 20:38:40');
 
--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -7,57 +7,91 @@ const pool = new Pool({
     host: 'localhost',
     user: 'postgres',
     password:'canito',
-    database: 'comisaria',
+    database: 'postgres',
     port:'5432' 
 })
 
 const getPermisos = async(req,res) => {
     const response = await pool.query('SELECT * FROM permisos').catch(function (error) {
         if (error.response) {
-          console.log(error.response.data);
-          console.log(error.response.status);
-          console.log(error.response.headers);
+            res.json({
+                message: 'Ocurrio un error'
+            });
         }
       });
     res.status(200).json(response)
 }
 
 const createPermiso = async (req,res)=> {
-    const {run,nombre,direccion,motivo,email,fecha} = req.body;
-    const resp = await pool.query('INSERT INTO permisos VALUES (DEFAULT,$1,$2,$3,$4,$5,$6)',[run,nombre,direccion,motivo,email,fecha]).catch(function (error) {
-        if (error.response) {
-            console.log(error.response.data);
-            console.log(error.response.status);
-            console.log(error.response.headers);
-        }
-        });
-    const resid = await pool.query('SELECT id FROM permisos WHERE run =$1 AND nombre=$2 AND direccion=$3 LIMIT 1',[run,nombre,direccion]).catch(function (error) {
-        if (error.response) {
-            console.log(error.response.data);
-            console.log(error.response.status);
-            console.log(error.response.headers);
-        }
-        });    
-    id = resid.rows[0].id
-    const permiso = generarPdf(req,id)
-    if(sendPermiso(email,permiso)){
-        res.json({
-            message: 'Permiso generado exitosamente!, su IDPermiso es: '+id,
-            body: {
-                permiso: {run,nombre,direccion,motivo,email}
+    const {run,nombre,direccion,motivo,email} = req.body;
+    const date = new Date();
+
+    if(verificarPermiso(req,date)){
+        const fecha_inicio = new Date(date.getTime()+15*60000);
+        const fecha_termino = new Date(fecha_inicio.getTime()+120*60000);
+
+        const resp = await pool.query('INSERT INTO permisos VALUES (DEFAULT,$1,$2,$3,$4,$5,$6,$7)',[run,nombre,direccion,motivo,email,fecha_inicio,fecha_termino]).catch(function (error) {
+            if (error.response) {
+                res.json({
+                    message: 'Ocurrio un error al generar su permiso'
+                });
             }
         });
+        const resid = await pool.query('SELECT id FROM permisos WHERE run =$1 AND nombre=$2 AND direccion=$3 AND fechaInicio=$4 LIMIT 1',[run,nombre,direccion,fecha_inicio]).catch(function (error) {
+            if (error.response) {
+                res.json({
+                    message: 'Ocurrio un error al generar su permiso'
+                });
+            }
+        });    
+        id = resid.rows[0].id
+        const permiso = generarPdf(req,id,fecha_inicio,fecha_termino)
+        if(sendPermiso(email,permiso)){
+            res.json({
+                message: 'Permiso generado exitosamente! \nID del permiso: '+id+'\nFecha de inicio: '+fecha_inicio.toString()+'\nFecha de termino: '+fecha_termino.toString(),
+                body: {
+                    permiso: {run,nombre,direccion,motivo,email}
+                }
+            });
+            }
+        else{
+            res.json({
+                message: 'Ocurrio un error al generar su permiso'
+            });
         }
-    else{
+    }else{
         res.json({
-            message: 'Ocurrio un error al generar su permiso'
+            message: 'Solo puede crear 1 permiso por dia!'
         });
     }
 };
 
 
-function generarPdf(req,id){
-    const {run,nombre,direccion,motivo,email,fecha} = req.body;
+function verificarPermiso (req,date){
+    /*
+    const {run,nombre,direccion,motivo,email} = req.body;
+    const resp = await pool.query('SELECT fechaInicio FROM permisos WHERE run =$1 ORDER BY id DESC LIMIT 1 ',[run]).catch(function (error) {
+        if (error.response) {
+            res.json({
+                message: 'Ocurrio un error al generar su permiso'
+            });
+        }
+    });
+    if(resp.rowCount== 0){
+        return true
+    }else{
+        console.log(resp)
+        console.log(resp.rows[0])
+        console.log(resp.rows[0].fechaInicio.toString())  
+    }
+    */
+   return true
+}
+
+
+
+function generarPdf(req,id,fecha_inicio,fecha_termino){
+    const {run,nombre,direccion,motivo,email} = req.body;
     const doc = new PDFDocument();
     doc.pipe(fs.createWriteStream('permiso'+run+'.pdf'));
     doc
@@ -75,12 +109,15 @@ function generarPdf(req,id){
     doc.text('Direccion: '+direccion, {
         width: 410,
         align: 'center'})
-    doc.text('Motivo'+motivo, {
+    doc.text('Motivo: '+motivo, {
         width: 410,
         align: 'center'})
-    doc.text('Fecha: '+fecha.toString(), {
+    doc.text('Fecha inicio: '+fecha_inicio.toString(), {
         width: 410,
         align: 'center'})
+    doc.text('Fecha termino: '+fecha_termino.toString(), {
+        width: 410,
+        align: 'center'})    
     doc.end();
     return 'permiso'+run+'.pdf'
 }
